82595d5dd6028a095d3fd6aabc6f1eacd9f1d91a
tomaszaba <tomas.zaba@outlook.com>
2025-04-03 23:13:09 +0200

    update script.R

---
+++ script.R
@@ -0,0 +1,30 @@
+################################################################################
+###                   TIME SERIES FORECASTING WORKFLOW                       ###
+################################################################################
+
+## ---- Load required libraries ------------------------------------------------
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(tsibble)
+library(feasts)
+library(ggplot2)
+library(fable)
+library(fabletools)
+library(sf)
+library(cyphr)
+
+## ---- Retrieve secret key ----------------------------------------------------
+secret_key <- data_key(".", Sys.getenv("path_secret_key"))
+
+## ---- Read project-specific functions ----------------------------------------
+lapply(list.files(path = "R", full.names = TRUE), FUN = source)
+
+## ---- Read data in -----------------------------------------------------------
+source("scripts/read-in-data.R")
+
+## ---- Data wrangling ---------------------------------------------------------
+source("scripts/data-wrangling.R")
+
+## ---- Exploratory Data Analysis ----------------------------------------------
+source("scripts/exploratory-data-analysis.R")
