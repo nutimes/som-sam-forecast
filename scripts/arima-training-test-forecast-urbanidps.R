942efa19fc46bdfc570e8de8a584f2c371b58165
tomaszaba <tomas.zaba@outlook.com>
2025-04-06 15:04:25 +0200

    check ACF and PACF and identify appropriate candidate models

--- scripts/arima-training-test-forecast-urbanidps.R
+++ scripts/arima-training-test-forecast-urbanidps.R
@@ -48,3 +48,19 @@ urbanidps_train_data |>
     axis.title.y = element_text(size = 10, margin = margin(r = 5)),
     plot.subtitle = element_text(colour = "#706E6D")
   )
+
+
+### ---------------------------------------------------- ACF and PACF plots ----
+
+urbanidps_train_data |>
+  mutate(
+    .admissions = do.call(
+      what = difference,
+      args = list(x = .admissions, lag = 12, differences = 1)
+    ) |> difference(1)
+  ) |>
+  gg_tsdisplay(y = .admissions, plot_type = "partial", lag_max = 36) +
+  labs(title = "Riverine livelihood system")
+
+# Candidate models selected based on ACF (MA) and PACF (AR):
+# ARIMA(0,1,1)(0,1,2)[12] or ARIMA(1,1,0)(0,1,0)
