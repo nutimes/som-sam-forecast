50574997d2fe8678701627f5c593491cac2b7b61
tomaszaba <tomas.zaba@outlook.com>
2025-04-06 15:19:50 +0200

    identify the best model: low AICc: .model=auto

--- scripts/arima-training-test-forecast-urbanidps.R
+++ scripts/arima-training-test-forecast-urbanidps.R
@@ -76,3 +76,34 @@ urbanidps_train_data |>
     ) |> difference(2)
   ) |>
   features(.var = .admissions, ljung_box, lag = 10)
+
+
+## ---- Fit a Seasonal ARIMA model ---------------------------------------------
+
+urbanidps_fit <- urbanidps_train_data |>
+  model(
+    sets = ETS(
+      formula = .admissions ~ error("M") + trend("Ad") + season("M")
+    ),
+    arima012010 = ARIMA(
+      formula = .admissions ~ pdq(0, 1, 2) + PDQ(0, 1, 0)
+    ),
+    arima110010 = ARIMA(
+      formula = .admissions ~ pdq(1, 1, 0) + PDQ(0, 1, 0)
+    ),
+    arima013010 = ARIMA(
+      formula = .admissions ~ pdq(0, 1, 3) + PDQ(0, 1, 0)
+    ),
+    auto = ARIMA(
+      formula = .admissions, stepwise = FALSE, approximation = FALSE
+    )
+  )
+
+
+### ----------------------- Identify the best model-fit amongst the others -----
+
+glance(urbanidps_fit) |>
+  arrange(AICc) |>
+  select(.model:BIC)
+
+# .model auto had the lowest AICc - best model.
